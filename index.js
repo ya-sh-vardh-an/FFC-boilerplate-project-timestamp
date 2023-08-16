462048162f33689d9a2b063ba5fdb49b776422dd
Emp <111492054+Yash5675@users.noreply.github.com>
2023-08-16 23:18:52 +0530

    feat: added get for api/1451001600000 link

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get("/api/2015-12-25", function (req, res) {
   res.json({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"});
 });
 
+// for the api/1451001600000 endpoint...
+app.get("/api/1451001600000", function (req, res) {
+  res.json({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
